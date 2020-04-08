d359370b843b5d5ece9fe469dac15b941ba02bfd
Vinicius Thiago <vtls@cin.ufpe.br>
2020-04-08 16:05:06 -0300

    Category

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.course.entities.Category;
 import com.example.course.entities.Order;
 import com.example.course.entities.User;
 import com.example.course.entities.enums.OrderStatus;
+import com.example.course.repositories.CategoryRepository;
 import com.example.course.repositories.OrderRepository;
 import com.example.course.repositories.UserRepository;
 
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override // Executa quand o programa for iniciado
 	public void run(String... args) throws Exception {
@@ -32,11 +37,16 @@ public class TestConfig implements CommandLineRunner {
 		
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.PAID);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WAITING_PAYMENT);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.WAITING_PAYMENT); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.WAITING_PAYMENT);
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
 		
 		//Salva objetos no banco de dados
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 	}
 
 }
---
+++ src/main/java/com/example/course/repositories/CategoryRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.example.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.course.entities.Category;
+
+/**
+ * @author vinic
+ * Repository responsavel por fazer operacoes com a entidade Category
+ */
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
