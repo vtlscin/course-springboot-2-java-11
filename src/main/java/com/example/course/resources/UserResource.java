1f9f71a377915019e7829ae2e849b3f7171d65e0
Vinicius Thiago <vtls@cin.ufpe.br>
2020-04-09 16:43:56 -0300

    User insert

--- src/main/java/com/example/course/resources/UserResource.java
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -3,14 +3,18 @@
  */
 package com.example.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.course.entities.User;
 import com.example.course.services.UserService;
@@ -40,6 +44,20 @@ public class UserResource {
 	{
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	/**
+	 * @param Uma resposta 201 retorna um uri que é o endereco do objeto inserido
+	 * @return
+	 */
+	@PostMapping()
+	public ResponseEntity<User> insert(@RequestBody User obj)
+	{
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 	
 }
