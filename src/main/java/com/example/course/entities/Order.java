baa24f9d3ff962759812dc3b94cca096a4792330
Vinicius Thiago <vtls@cin.ufpe.br>
2020-04-09 16:26:20 -0300

    Subtotal & Total methods

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -104,6 +104,17 @@ public class Order implements Serializable{
 		return items;
 	}
 	
+	public Double getTotal()
+	{
+		double sum = 0.0;
+		
+		for (OrderItem item : items)
+		{
+			sum += item.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
