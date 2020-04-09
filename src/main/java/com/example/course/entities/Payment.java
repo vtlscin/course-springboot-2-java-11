91434c0a83837e3d2fbea8a33da40164ea8f5539
Vinicius Thiago <vtls@cin.ufpe.br>
2020-04-09 16:16:37 -0300

    Payment, one-to-one association

---
+++ src/main/java/com/example/course/entities/Payment.java
@@ -0,0 +1,89 @@
+package com.example.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long Id;
+	private Instant moment;
+
+	@OneToOne
+	@MapsId
+	private Order order;
+	
+	public Payment() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Payment(Long id, Instant moment, Order order) {
+		super();
+		Id = id;
+		this.moment = moment;
+		this.order = order;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+
+}
